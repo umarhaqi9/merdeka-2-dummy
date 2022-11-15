f6bbb420ad948bcccc8b514dca3c932dd23e265a
umarhaqi9 <74255063+umarhaqi9@users.noreply.github.com>
2022-11-15 11:44:46 +0700

    Initial sidebar

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,7 +5,24 @@ import { useOptionalUser } from "~/utils";
 export default function Index() {
   const user = useOptionalUser();
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
+    <>
+      
+      <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
+      
+      <div className=" bg-blue-600 font-sans">
+        <div className=" sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900">
+          <div className="text-gray-100 text-xl">
+            <div className="p-2 mt-1 flex items-center text-slate-100">
+              <svg role="img" className="fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <title>/e/</title>
+                <path d="M13.13812.05332C9.6246-.2791 6.0635.95029 3.49234 3.53409.0641 6.97916-.94746 12.17083.93662 16.65094c1.88408 4.48013 6.30246 7.38884 11.16248 7.3483a2.27493 2.27493 0 10-.03759-4.54888c-3.02833.02526-5.75722-1.77058-6.93123-4.56223-1.174-2.79164-.54918-5.99816 1.58702-8.14484 2.13619-2.14669 5.339-2.78735 8.13635-1.62703 2.07161.85929 3.59079 2.57925 4.24094 4.64104H12.0506a2.27505 2.27505 0 100 4.5501h9.67488a2.27493 2.27493 0 002.27445-2.26717c.0167-4.86014-2.91357-9.264-7.40286-11.12612-1.12232-.46552-2.28778-.74998-3.45895-.8608Z"/>
+              </svg>
+              <h1 className="font-bold text-gray-200">Universitas Multimedia Nusantara</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="relative sm:pb-16 sm:pt-8">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
@@ -134,5 +151,7 @@ export default function Index() {
         </div>
       </div>
     </main>
+    </>
+    
   );
 }
